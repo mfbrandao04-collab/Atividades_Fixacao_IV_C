4ddb2ad60b48d69f07d8287fc069b6d1d5df9174
mfbrandao04-collab <mfbrandao04@gmail.com>
2026-09-15 15:24:14 -0400

    Add missing options and fix syntax errors in main

--- questao02.c
+++ questao02.c
@@ -1,12 +1,13 @@
 #include <stdio.h> 
 int main (){
+
 int opcao;
 printf ("1 - Saldo\n");
 printf ("2 - Extrato\n");
 printf ("3 - Transferencia\n");
 printf ("4 - Pagamento\n");
-printf ("5- Emprestimo\n");
-printf ("6- Encerrar\n");
+printf ("5 - Emprestimo\n");
+printf ("6 - Encerrar\n");
 
 printf ("Informe o tipo de atendimento: \n ");
 scanf ("%d", &opcao);
@@ -15,15 +16,31 @@ switch (opcao) {
     case 1:
         printf ("Saldo selecionado. ");
         break;
+
     case 2: 
         printf ("Extrato selecionado. ");
         break;
+
     case 3: 
         printf ("Transferencia selecionada. ");
         break;
+
     case 4:
         printf ("Pagamento selecionado. ");
-        break
+        break;
+
+    case 5:
+        printf ("Emprestimo selecionado. ");
+        break;
+
+    case 6:
+        printf ("Encerrando o programa. ");
+        break;
+
+    default:
+        printf ("Opcao invalida. ");
+        break;
     }
+    return 0;
 
 }
